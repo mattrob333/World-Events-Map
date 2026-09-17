576594948dad040014b80aaea21bb92cbad44791
Matt Roberson <31228129+mattrob333@users.noreply.github.com>
2026-09-17 13:54:07 -0400

    feat: export traveler profile utilities

---
+++ src/lib/profile/index.ts
@@ -0,0 +1,25 @@
+export {
+  detectContentProvider,
+  displayHost,
+  normalizeExternalUrl,
+  youtubeVideoId,
+  type TravelContentProvider,
+} from './content';
+export {
+  profileCompleteness,
+  profileMilestones,
+  type ProfileInventory,
+  type ProfileMilestone,
+} from './completeness';
+export type {
+  ProfileLinkKind,
+  ProfilePlaceKind,
+  ProfileTheme,
+  PublicProfileContent,
+  PublicProfileLink,
+  PublicProfilePlace,
+  PublicTravelMode,
+  PublicTravelerProfile,
+  TravelerProfileDraft,
+  TravelerSnapshot,
+} from './types';
